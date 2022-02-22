4f89adcae302b98232fb594e01aa727df522c270
312C-26 <312C-26@312C-26>
2022-02-21 20:29:53 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio15/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pesoA=0 , pesoB=0 , pesoC=0 , pesoD=0  ;
+            if ((pesoA == pesoB) && (pesoA == pesoC))
+            {
+                if(pesoD > pesoA)
+                {
+                    Console.WriteLine("La esfera D es la diferente y");
+                }
+                else
+                {
+                    Console.WriteLine("La esfera D es la diferente y");
+                }
+
+            }
+            else
+            {
+                if ((pesoA == pesoB) && (pesoA == pesoD))
+                {
+                    Console.WriteLine("La esfera C es la diferente");
+                    if (pesoC > pesoA)
+                    {
+                        Console.WriteLine("y es de mayor peso");
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("y es de menor peso");
+                    }
+
+
+                }
+                else
+                {
+                    if((pesoA == pesoC) && (pesoA == pesoD))
+                    {
+                        Console.WriteLine("La esfera B es la diferente");
+                        if (pesoB > pesoD)
+                        {
+                            Console.WriteLine("y es de mayor peso");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("y es de mayor peso");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("La esfera A es la Diferente");
+                        if (pesoA > pesoB)
+                        {
+                            Console.WriteLine("y es de mayor peso");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("y es de menor peso");
+                        }
+                    }
+
+
+                    
+                }
+            }
+        }
+    }
+}
